aed56cd72588cef7d6171c28911c69b0981a9109
Nelson Yanes <nelsonyanes@Nelsons-MacBook-Air.local>
2015-11-01 23:47:43 -0500

    finished 5

--- HW4/reinforcement/qlearningAgents.py
+++ HW4/reinforcement/qlearningAgents.py
@@ -115,11 +115,12 @@ class QLearningAgent(ReinforcementAgent):
         action = None
         "*** YOUR CODE HERE ***"
 
+
         if not util.flipCoin(self.epsilon):
-          return self.computeActionFromQValues(state) #Take best policy action
+            return self.computeActionFromQValues(state) #Take best policy action
         else:
           if len(legalActions) != 0:
-            return random.choice(legalActions)[1] #Take random action
+            return random.choice(legalActions) #Take random action
           else:
             return None
 
